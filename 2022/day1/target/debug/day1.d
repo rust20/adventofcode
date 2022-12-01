b4e348eb139103e6c424236a7d509f4ee82dfc81
Azka Ali <azkaali@ruangguru.com>
2022-12-02 01:56:25 +0700

    remove target, add gitignore, finish day 1 again??

--- 2022/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/rust20/gits/adventofcode/2022/day1/target/debug/day1: /home/rust20/gits/adventofcode/2022/day1/src/main.rs
--- 2022/day1/target/debug/deps/day1-f46fd56b4b9881a9.d
+++
@@ -1,5 +0,0 @@
-/home/rust20/gits/adventofcode/2022/day1/target/debug/deps/day1-f46fd56b4b9881a9.rmeta: src/main.rs
-
-/home/rust20/gits/adventofcode/2022/day1/target/debug/deps/day1-f46fd56b4b9881a9.d: src/main.rs
-
-src/main.rs:
