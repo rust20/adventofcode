03f52aef37cb477bb1ddcc6173554095bf9f661a
Azka Ali <azkaali@ruangguru.com>
2022-12-04 12:59:44 +0700

    day4 part 2

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -33,6 +33,31 @@ fn part1() {
 
     println!("{}", sum);
 }
+
+fn part2() {
+    let input_raw = get_input();
+    let input = input_raw.split("\n");
+
+    let mut sum: i32 = 0;
+
+    for pair in input.into_iter() {
+        if pair == "" {
+            continue;
+        }
+        let pairs: Vec<Vec<i32>> = pair
+            .split(",")
+            .map(|x| x.split("-").map(|x| x.parse::<i32>().unwrap()).collect())
+            .collect();
+
+        if !(pairs[0][1] < pairs[1][0] || pairs[0][0] > pairs[1][1]) {
+            sum += 1;
+        }
+    }
+
+    println!("{}", sum);
+}
+
 fn main() {
     part1();
+    part2();
 }
