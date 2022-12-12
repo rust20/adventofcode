4ab9d4d5c59824a4952c9cc7f2f9b7e5e9bd4e2c
Azka Ali <azkaali@ruangguru.com>
2022-12-13 01:37:38 +0700

    day9 part2

--- 2022/day9/src/main.rs
+++ 2022/day9/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 use std::fs::File;
+use std::io;
 use std::io::{BufReader, Read};
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
@@ -71,6 +72,31 @@ fn part1(input_raw: String) -> i32 {
     loc_set.len() as i32
 }
 
+#[allow(dead_code)]
+fn print_vec_tup(x: i32, y: i32, t: &Vec<Tuple>) {
+    let mut map = vec![vec![" "; x as usize]; y as usize];
+    let k = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    for val in (0..t.len()).rev() {
+        map[(t[val].x + x / 2) as usize][(t[val].y + y / 2) as usize] = k[val].clone();
+    }
+
+    for j in (0..y).rev() {
+        for i in 0..x {
+            print!("{}", map[i as usize][j as usize]);
+        }
+        println!("");
+    }
+    println!("");
+    println!("");
+
+    let mut user_input = String::new();
+    let stdin = io::stdin(); // We get `Stdin` here.
+    let _ = stdin.read_line(&mut user_input);
+
+    println!("input {} ", user_input);
+}
+
 fn move_tail2(head: &Tuple, tail: &Tuple) -> Tuple {
     match (head, tail) {
         (h, t) if h.x == t.x - 1 && h.y == t.y + 2 => Tuple::new(t.x - 1, t.y + 1),
@@ -89,10 +115,10 @@ fn move_tail2(head: &Tuple, tail: &Tuple) -> Tuple {
         (h, t) if h.x == t.x - 2 && h.y == t.y + 0 => Tuple::new(t.x - 1, t.y + 0),
         (h, t) if h.x == t.x - 2 && h.y == t.y + 1 => Tuple::new(t.x - 1, t.y + 1),
 
-        (h, t) if h.x == t.x + 1 && h.y == t.y + 1 => Tuple::new(t.x + 1, t.y + 1),
-        (h, t) if h.x == t.x + 1 && h.y == t.y - 1 => Tuple::new(t.x + 1, t.y - 1),
-        (h, t) if h.x == t.x - 1 && h.y == t.y + 1 => Tuple::new(t.x - 1, t.y + 1),
-        (h, t) if h.x == t.x - 1 && h.y == t.y - 1 => Tuple::new(t.x - 1, t.y - 1),
+        (h, t) if h.x == t.x + 2 && h.y == t.y + 2 => Tuple::new(t.x + 1, t.y + 1),
+        (h, t) if h.x == t.x + 2 && h.y == t.y - 2 => Tuple::new(t.x + 1, t.y - 1),
+        (h, t) if h.x == t.x - 2 && h.y == t.y + 2 => Tuple::new(t.x - 1, t.y + 1),
+        (h, t) if h.x == t.x - 2 && h.y == t.y - 2 => Tuple::new(t.x - 1, t.y - 1),
 
         _ => tail.clone(),
     }
@@ -112,6 +138,7 @@ fn part2(input_raw: String) -> i32 {
         let dir = args[0];
         let count: i32 = args[1].parse().unwrap();
         for _ in 0..count {
+            // print_vec_tup(100, 100, &rope);
             rope[0] = match dir {
                 "R" => Tuple::new(rope[0].x + 1, rope[0].y),
                 "L" => Tuple::new(rope[0].x - 1, rope[0].y),
@@ -121,11 +148,12 @@ fn part2(input_raw: String) -> i32 {
             };
 
             for i in 1..rope_length {
-                rope[i] = move_tail(&rope[i - 1], &rope[i])
+                rope[i] = move_tail2(&rope[i - 1], &rope[i])
             }
-            loc_set.insert(rope.last().unwrap().clone());
+            loc_set.insert(rope[9].clone());
         }
     }
+
     loc_set.len() as i32
 }
 
@@ -148,6 +176,10 @@ mod tests {
 
     #[test]
     fn test_part2_case_1() {
-        assert_eq!(part2(get_input("input_test.txt")), 36);
+        assert_eq!(part2(get_input("input_test.txt")), 1);
+    }
+    #[test]
+    fn test_part2_case_2() {
+        assert_eq!(part2(get_input("input_test2.txt")), 36);
     }
 }
