65cac06415e7574f98cccc07e7faebe81e0b46a9
Azka Ali <azkaali@ruangguru.com>
2022-12-12 11:43:32 +0700

    day7 working but not correct

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -1,45 +1,39 @@
-use std::borrow::BorrowMut;
-// use regex::Regex;
-use std::cell::{RefCell, RefMut};
-// use std::collections::HashMap;
+use std::cell::RefCell;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::rc::Rc;
 
-type Node = Rc<RefCell<Box<FileType>>>;
-
-#[derive(Clone, Debug)]
-struct Directory {
-    name: String,
-    children: Vec<Node>,
-}
-
 #[derive(Clone, Debug)]
 enum FileType {
-    Directory(Directory),
+    Directory(String),
     File(String, u32),
 }
 
+type Node = Rc<RefCell<TreeNode>>;
+
+#[derive(Clone, Debug)]
 struct TreeNode {
     val: FileType,
-    children: Vec<Rc<RefCell<TreeNode>>>,
+    children: Vec<Node>,
 }
 
 impl TreeNode {
-    pub fn new(ft: FileType) -> Self {
-        Self {
+    pub fn new(ft: FileType) -> Node {
+        let new = TreeNode {
             val: ft,
             children: Vec::new(),
-        }
+        };
+        Rc::new(RefCell::new(new))
     }
 }
 
 const LIMIT: u32 = 100000;
 
 fn traverse_limit(node: Node) -> u32 {
-    match *node.as_ref().borrow().clone() {
-        FileType::Directory(d) => d.children.iter().fold(0, |acc, val| {
-            let res = traverse_limit(val.clone());
+    let tree_node = node.as_ref().borrow();
+    match tree_node.val {
+        FileType::Directory(_) => tree_node.children.iter().fold(0, |acc, val| {
+            let res = traverse_limit(Rc::clone(val));
             acc + (if res <= LIMIT { res } else { 0 })
         }),
         FileType::File(_, f) => {
@@ -67,74 +61,87 @@ fn part1(input_raw: String) -> u32 {
 
     let mut stack = Vec::new();
 
-    let root_dir = FileType::Directory(Directory {
-        name: "/".to_string(),
-        children: Vec::new(),
-    });
-    let root = Rc::new(RefCell::new(Box::new(root_dir)));
-    let mut curr = Rc::clone(&root);
+    let mut sum = 0;
 
+    let traverse = |node| {
+        let tree_node = node.as_ref().borrow();
+        match tree_node.val {
+            FileType::Directory(_) => tree_node.children.iter().fold(0, |acc, val| {
+                let res = traverse_limit(Rc::clone(val));
+                acc + (if res <= LIMIT { res } else { 0 })
+            }),
+            FileType::File(_, f) => {
+                if f <= LIMIT {
+                    f
+                } else {
+                    0
+                }
+            }
+        }
+    };
+
+    let root = TreeNode::new(FileType::Directory("/".to_string()));
+
+    let mut curr = Rc::clone(&root);
     while let Some(i) = command_iter.next() {
-        println!("{:?}", i);
+        println!("{:?} {:?}", i, 0);
+        if i == "" {
+            break;
+        }
         let args: Vec<&str> = i.split(" ").collect();
 
         if args[0] != "$" {
             let filename = args[1].to_string();
-            let filename_ = filename.clone();
             if args[0] == "dir" {
-                if let FileType::Directory(mut dir) = *curr.as_ref().borrow_mut().clone() {
-                    let ft = FileType::Directory(Directory {
-                        name: filename,
-                        children: Vec::new(),
-                    });
-                    dir.children.push(Rc::new(RefCell::new(Box::new(ft))));
-                    println!("push dir {} {:?}", filename_, dir.children);
-                }
+                curr.as_ref()
+                    .borrow_mut()
+                    .children
+                    .push(TreeNode::new(FileType::Directory(filename)));
                 continue;
             }
 
             let filesize = args[0].parse::<u32>().unwrap();
 
-            if let FileType::Directory(mut dir) = *curr.as_ref().borrow_mut().clone() {
-                let ft = FileType::File(filename, filesize);
-                dir.children.push(Rc::new(RefCell::new(Box::new(ft))));
-                println!("push file {} {:?}", filename_, dir.children);
-            }
+            curr.as_ref()
+                .borrow_mut()
+                .children
+                .push(TreeNode::new(FileType::File(filename, filesize)));
             continue;
         }
 
-        if args[0] == "$" && args[1] == "ls" {
+        if args[1] == "ls" {
             continue;
         }
 
-        if args[0] == "$" && args[1] == "cd" {
-            if args[2] == "/" {
-                continue;
-            }
-            if args[2] == ".." {
-                curr = stack.pop().unwrap();
-                continue;
-            }
-            stack.push(Rc::clone(&curr));
-            let mut a = Rc::clone(&curr);
-            if let FileType::Directory(dir) = *curr.as_ref().borrow().clone() {
-                let childs = dir.children;
-                println!("{:?}", childs);
-                let tmp = childs
-                    .iter()
-                    .find(|x| {
-                        if let FileType::Directory(d) = *x.as_ref().borrow().clone() {
-                            d.name == args[2].to_string()
-                        } else {
-                            false
-                        }
-                    })
-                    .unwrap();
-                a = Rc::clone(&tmp);
-            }
-            curr = a;
+        if !(args[1] == "cd") {
             continue;
         }
+        if args[2] == "/" {
+            continue;
+        }
+        if args[2] == ".." {
+            curr = stack.pop().unwrap();
+            continue;
+        }
+        stack.push(Rc::clone(&curr));
+
+        let curr_cloned = Rc::clone(&curr);
+        let curr_borrowed = curr_cloned.borrow();
+        let result2 = curr_borrowed
+            .children
+            .iter()
+            .find(|x| {
+                if let FileType::Directory(dir) = x.as_ref().borrow_mut().val.clone() {
+                    dir == args[2]
+                } else {
+                    false
+                }
+            })
+            .unwrap();
+
+        curr = Rc::clone(result2);
+
+        continue;
     }
 
     traverse_limit(root)
