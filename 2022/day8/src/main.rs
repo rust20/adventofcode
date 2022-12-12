31e9a2e59051becca5ad2801bf82511d392a7bdf
Azka Ali <azkaali@ruangguru.com>
2022-12-12 21:02:58 +0700

    day8 part2

--- 2022/day8/src/main.rs
+++ 2022/day8/src/main.rs
@@ -17,11 +17,34 @@ struct Map {
 }
 
 impl Map {
-    pub fn new() -> Self {
+    pub fn new(source: String) -> Self {
+        let tree_map: Vec<Vec<i32>> = source
+            .split("\n")
+            .filter(|x| !x.is_empty())
+            .map(|x| {
+                x.split("")
+                    .filter(|x| !x.is_empty())
+                    .map(|x| x.parse::<i32>().unwrap())
+                    .collect()
+            })
+            .collect();
+
+        let row_max = tree_map.len() - 1;
+        let col_max = tree_map[0].len() - 1;
+
+        let visibility_map = vec![vec![false; col_max + 1]; row_max + 1];
+
         Self {
-            trees: Vec::new(),
-            visible: Vec::new(),
+            trees: tree_map,
+            visible: visibility_map,
         }
+    }
+
+    pub fn get_map_size(&self) -> (i32, i32) {
+        (
+            (self.trees.len() - 1) as i32,
+            (self.trees[0].len() - 1) as i32,
+        )
     }
 
     pub fn tget(&self, loc: Tup) -> i32 {
@@ -37,13 +60,11 @@ impl Map {
         let col_max = (self.trees[0].len() - 1) as i32;
 
         let mut min = self.tget(s);
-        // println!("marking {:?} {:?} {} {}", s, d, min, 0);
 
         s.0 += d.0;
         s.1 += d.1;
         while 0 < s.0 && s.0 < row_max && 0 < s.1 && s.1 < col_max {
             let curr = self.tget(s);
-            // println!("check {:?} {} {}", s, min, curr);
 
             if curr > min {
                 self.set_visible(s);
@@ -78,39 +99,47 @@ impl Map {
             println!()
         })
     }
+
+    pub fn get_scenic_score(&self, loc: Tup) -> i32 {
+        let dir: Vec<Tup> = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
+        dir.iter()
+            .fold(1, |acc, dir| acc * self.scenic_dir(loc, *dir))
+    }
+
+    fn scenic_dir(&self, mut s: Tup, d: Tup) -> i32 {
+        let (row_max, col_max) = self.get_map_size();
+
+        let mut score = 1;
+        let max = self.tget(s);
+
+        s.0 += d.0;
+        s.1 += d.1;
+        while 0 < s.0 && s.0 < row_max && 0 < s.1 && s.1 < col_max {
+            if self.tget(s) >= max {
+                break;
+            }
+            score += 1;
+            s.0 += d.0;
+            s.1 += d.1;
+        }
+
+        score
+    }
 }
 
 fn part1(input_raw: String) -> i32 {
-    let input: Vec<Vec<i32>> = input_raw
-        .split("\n")
-        .filter(|x| !x.is_empty())
-        .map(|x| {
-            x.split("")
-                .filter(|x| !x.is_empty())
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect()
-        })
-        .collect();
-
-    let row_max = input.len() - 1;
-    let col_max = input[0].len() - 1;
-
-    let bool_map = vec![vec![false; col_max + 1]; row_max + 1];
-
-    let mut map = Map::new();
-    map.trees = input;
-    map.visible = bool_map;
-
-    let dir = ((1, 0), (-1, 0), (0, 1), (0, -1));
+    let mut map = Map::new(input_raw);
+    let (row_max, col_max) = map.get_map_size();
+    let dir: Vec<Tup> = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
 
     for i in 1..row_max {
-        map.mark((i as i32, 0), dir.2);
-        map.mark((i as i32, row_max as i32), dir.3);
+        map.mark((i as i32, 0), dir[2]);
+        map.mark((i as i32, row_max), dir[3]);
     }
 
     for i in 1..col_max {
-        map.mark((0, i as i32), dir.0);
-        map.mark((col_max as i32, i as i32), dir.1);
+        map.mark((0, i as i32), dir[0]);
+        map.mark((col_max, i as i32), dir[1]);
     }
 
     // map.print_vis();
@@ -120,9 +149,21 @@ fn part1(input_raw: String) -> i32 {
 }
 
 fn part2(input_raw: String) -> i32 {
-    let input = input_raw.chars();
-    let res = input.count();
-    res as i32
+    let map = Map::new(input_raw);
+    let (row_max, col_max) = map.get_map_size();
+
+    let mut max_score = 0;
+
+    for i in 0..=row_max {
+        for j in 0..=col_max {
+            let score = map.get_scenic_score((i, j));
+            if score > max_score {
+                max_score = score;
+            }
+        }
+    }
+
+    max_score
 }
 
 fn main() {
