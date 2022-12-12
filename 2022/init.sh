bdd7995acb5ed32694cee195b4b498c690978b8f
Azka Ali <azkaali@ruangguru.com>
2022-12-12 23:50:20 +0700

    update template

--- 2022/init.sh
+++ 2022/init.sh
@@ -5,6 +5,6 @@ local name=$1
 echo initializing $name
 mkdir $1
 cd $1
-touch input.txt problem.md
+touch input.txt input_test.txt problem.md
 cargo init --vcs none
 cp ../template.rs src/main.rs
--- 2022/template.rs
+++ 2022/template.rs
@@ -1,9 +1,9 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
 
-fn get_input() -> String {
+fn get_input(file_name: &str) -> String {
     let mut data = String::new();
-    let f = File::open("input.txt").expect("Unable to open file");
+    let f = File::open(file_name).expect("Unable to open file");
     let mut br = BufReader::new(f);
     br.read_to_string(&mut data).expect("Unable to read string");
     data
@@ -22,7 +22,7 @@ fn part2(input_raw: String) -> i32 {
 }
 
 fn main() {
-    let input_raw = get_input();
+    let input_raw = get_input("input.txt");
 
     let res1 = part1(input_raw.clone());
     let res2 = part2(input_raw);
@@ -35,6 +35,6 @@ mod tests {
     use super::*;
     #[test]
     fn test_part1_case_1() {
-        assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string()), 5);
+        assert_eq!(part1(get_input("input_test.txt")), 5);
     }
 }
