63bb5774e8c4fecc64d8d1b9332598da9d5b61ab
Azka Ali <azkali20@gmail.com>
2023-12-21 11:18:27 +0100

    solve day18

--- 2023/src/bin/d18.rs
+++ 2023/src/bin/d18.rs
@@ -1,12 +1,111 @@
 use std::{collections::HashMap, time::Instant};
 
+
 pub fn part1(inp: &str) -> i64 {
-    let sum = inp.len();
+
+    let mut points = vec![];
+
+    for line in inp.lines() {
+        let mut k = line.split(" ");
+        let dir = k.next().unwrap();
+        let len = k.next().unwrap().parse::<isize>().unwrap();
+        let color = k.next().unwrap();
+
+        points.push((dir, len, color));
+    }
+
+    points.push(points[0]);
+    points.push(points[1]);
+
+    let mut prev = (0_isize, 0_isize);
+    let mut points_v2 = vec![];
+    for i in 0..points.len() - 2 {
+        let trip = &points[i..i + 3];
+        let len = trip[1].1;
+        let pos = match [trip[0].0, trip[1].0, trip[2].0] {
+            ["R", "U", "L"] => (prev.0, prev.1 - (len - 1)),
+            ["L", "U", "R"] => (prev.0, prev.1 - (len + 1)),
+            ["R", "D", "L"] => (prev.0, prev.1 + (len + 1)),
+            ["L", "D", "R"] => (prev.0, prev.1 + (len - 1)),
+
+            ["U", "R", "D"] => (prev.0 + (len+1), prev.1),
+            ["D", "R", "U"] => (prev.0 + (len-1), prev.1),
+            ["U", "L", "D"] => (prev.0 - (len-1), prev.1),
+            ["D", "L", "U"] => (prev.0 - (len+1), prev.1),
+
+            ["R", "U", "R"] | ["L", "U", "L"] => (prev.0, prev.1 - len),
+            ["R", "D", "R"] | ["L", "D", "L"] => (prev.0, prev.1 + len),
+            ["U", "R", "U"] | ["D", "R", "D"] => (prev.0 + len , prev.1),
+            ["U", "L", "U"] | ["D", "L", "D"] => (prev.0 - len , prev.1),
+            _ => unreachable!(),
+        };
+
+        prev = pos;
+        points_v2.push(pos);
+    }
+
+    let mut area = 0;
+    for i in 0..points_v2.len() {
+        let sec = (i + 1) % points_v2.len();
+        area += points_v2[i].0 * points_v2[sec].1;
+        area -= points_v2[i].1 * points_v2[sec].0;
+    }
+
+    let sum = area / 2;
     sum as i64
 }
 
 pub fn part2(inp: &str) -> i64 {
-    let sum = inp.len();
+    let mut points = vec![];
+
+    for line in inp.lines() {
+        let mut k = line.split("#");
+        let _ = k.next();
+        let color = k.next().unwrap();
+        let len = isize::from_str_radix(&color[..5], 16).unwrap();
+        let dir = &color[5..6];
+
+        points.push((dir, len, color));
+    }
+
+    points.push(points[0]);
+    points.push(points[1]);
+
+    let mut prev = (0_isize, 0_isize);
+    let mut points_v2 = vec![];
+    for i in 0..points.len() - 2 {
+        let trip = &points[i..i + 3];
+        let len = trip[1].1;
+        let pos = match [trip[0].0, trip[1].0, trip[2].0] {
+            ["0", "3", "2"] => (prev.0, prev.1 - (len - 1)),
+            ["2", "3", "0"] => (prev.0, prev.1 - (len + 1)),
+            ["0", "1", "2"] => (prev.0, prev.1 + (len + 1)),
+            ["2", "1", "0"] => (prev.0, prev.1 + (len - 1)),
+
+            ["3", "0", "1"] => (prev.0 + (len+1), prev.1),
+            ["1", "0", "3"] => (prev.0 + (len-1), prev.1),
+            ["3", "2", "1"] => (prev.0 - (len-1), prev.1),
+            ["1", "2", "3"] => (prev.0 - (len+1), prev.1),
+
+            ["0", "3", "0"] | ["2", "3", "2"] => (prev.0, prev.1 - len),
+            ["0", "1", "0"] | ["2", "1", "2"] => (prev.0, prev.1 + len),
+            ["3", "0", "3"] | ["1", "0", "1"] => (prev.0 + len , prev.1),
+            ["3", "2", "3"] | ["1", "2", "1"] => (prev.0 - len , prev.1),
+            _ => unreachable!(),
+        };
+
+        prev = pos;
+        points_v2.push(pos);
+    }
+
+    let mut area = 0;
+    for i in 0..points_v2.len() {
+        let sec = (i + 1) % points_v2.len();
+        area += points_v2[i].0 * points_v2[sec].1;
+        area -= points_v2[i].1 * points_v2[sec].0;
+    }
+
+    let sum = area / 2;
     sum as i64
 }
 
@@ -14,12 +113,12 @@ pub fn part2(inp: &str) -> i64 {
 fn main() {
     #[rustfmt::skip]
     let inputs = vec![
-        // "input1.txt",
+        "input1.txt",
         "input2.txt",
     ];
 
     for fi in inputs {
-        let path = "tests/d15/".to_string();
+        let path = "tests/d18/".to_string();
         let reader = std::fs::read_to_string(path + fi).expect("read input");
 
         let start1 = Instant::now();
